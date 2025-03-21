bc096aecdcd036703f0cc110971fe38b22f31995
Jan Van Look <jan.derpingguys@hotmail.com>
2025-03-21 02:04:44 +0100

    updates

---
+++ lua/plugins/discord.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "vyfor/cord.nvim",
+    build = ":Cord update",
+    opts = function()
+      return {
+        display = {
+          theme = "catppuccin",
+          flavor = "accent",
+        },
+        text = {
+          workspace = function(opts)
+            local hour = tonumber(os.date("%H"))
+            local status = hour >= 22 and "🌙 Late night coding"
+              or hour >= 18 and "🌆 Evening session"
+              or hour >= 12 and "☀️ Afternoon coding"
+              or hour >= 5 and "🌅 Morning productivity"
+              or "🌙 Midnight hacking"
+
+            return string.format("%s: %s", status, opts.filename)
+          end,
+        },
+      }
+    end,
+  },
+}
