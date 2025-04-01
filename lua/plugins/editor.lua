38cd16608a522ec08e89ee5ed3dd444b7d1933f2
Jan Van Look <jan.derpingguys@hotmail.com>
2025-04-02 01:45:30 +0200

    add gopher plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -19,4 +19,23 @@ return {
       },
     },
   },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    -- branch = "develop", -- if you want develop branch
+    -- keep in mind, it might break everything
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
+    },
+    -- (optional) will update plugin's deps on every update
+    build = ":GoInstallDeps",
+    keys = {
+      { "<leader>cg", desc = "Go" },
+      { "<leader>cge", "<cmd>GoIfErr<cr>", desc = "if err != nil" },
+      { "<leader>cgs", desc = "Go Structs" },
+      { "<leader>cgsj", "<cmd>GoTagAdd json<cr>", desc = "Go Add Json Tags" },
+    },
+  },
 }
