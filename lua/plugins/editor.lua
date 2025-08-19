6d7598baa0cbcc3e98c797b14aff777cedbb144e
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-19 17:54:50 +0200

    remove neo-tree

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    config = function()
-      require("neo-tree").setup({
-        window = {
-          width = 25,
-        },
-      })
-    end,
-  },
-  {
     "nvim-telescope/telescope.nvim",
     keys = {
       {
