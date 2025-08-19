8b97d50373ad69b060c87ca9692ef662bdd65294
Jan Van Look <jan.derpingguys@hotmail.com>
2025-08-19 17:57:13 +0200

    add kanagawa theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -64,9 +64,47 @@ return {
     priority = 1000,
     opts = {
       styles = {
-        transparency = true,
+        transparency = false,
       },
     },
+  },
+
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = true,
+    priority = 1000,
+    -- opts = {
+    --   transparent = true,
+    --   theme = "dragon", -- can choose between wave, dragon and lotus
+    --   colors = {
+    --     theme = {
+    --       all = {
+    --         ui = {
+    --           bg_gutter = "none",
+    --         },
+    --       },
+    --     },
+    --   },
+    --   overrides = function(colors)
+    --     local theme = colors.theme
+    --     return {
+    --       NormalFloat = { bg = "none" },
+    --       FloatBorder = { bg = "none" },
+    --       FloatTitle = { bg = "none" },
+
+    --       -- Save an hlgroup with dark background and dimmed foreground
+    --       -- so that you can use it where your still want darker windows.
+    --       -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+    --       NormalDark = { fg = theme.ui.fg_dim, bg = "none" },
+
+    --       -- Popular plugins that open floats will link to NormalFloat by default;
+    --       -- set their background accordingly if you wish to keep them dark and borderless
+    --       LazyNormal = { bg = "none", fg = theme.ui.fg_dim },
+    --       MasonNormal = { bg = "none", fg = theme.ui.fg_dim },
+    --     }
+    --   end,
+    -- },
   },
 
   {
