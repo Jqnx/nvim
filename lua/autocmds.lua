73107000fb2d8f9965fd3cf0b04bec35520792df
Jan Van Look <jan.derpingguys@hotmail.com>
2026-03-02 12:33:25 +0100

    update padding change

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,7 +3,7 @@ require("nvchad.autocmds")
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("VimEnter", {
-	command = ":silent !kitty @ set-spacing padding=0 margin=0",
+	command = ":silent !kitty @ set-spacing padding-left=0 padding-top=0 padding-right=0 margin=0",
 })
 
 autocmd("VimLeavePre", {
