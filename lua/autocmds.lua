0efb38d3cbd419563d628e11880e0e37ec136ae2
Jan Van Look <jan.derpingguys@hotmail.com>
2026-03-21 11:08:00 +0100

    changed kitty to alacritty

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,11 +3,13 @@ require("nvchad.autocmds")
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("VimEnter", {
-	command = ":silent !kitty @ set-spacing padding-left=0 padding-top=0 padding-right=0 margin=0",
+	-- command = ":silent !kitty @ set-spacing padding-left=0 padding-top=0 padding-right=0 margin=0",
+	command = ":silent !alacritty msg config 'window.padding={x=0,y=0}'",
 })
 
 autocmd("VimLeavePre", {
-	command = ":silent !kitty @ set-spacing padding=10 margin=0",
+	-- command = ":silent !kitty @ set-spacing padding=10 margin=0",
+	command = ":silent !alacritty msg config 'window.padding={x=10,y=5}'",
 })
 
 --autocmd({ "BufWritePost" }, {
