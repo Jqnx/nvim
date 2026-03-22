63636b159ca5ddccb551cab6285c80dbf8043bce
Jan Van Look <jan.derpingguys@hotmail.com>
2026-03-22 16:12:05 +0100

    added windsurf autocomplete

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -7,9 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map({ "n", "v"}, "q", "b")
-map({ "n", "v"}, "b", "q")
+map({ "n", "v" }, "q", "b")
+map({ "n", "v" }, "b", "q")
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "quit all"})
-map("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "telescope find files"})
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "quit all" })
+map("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
 map("n", "<leader>ll", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>ct", "<cmd>Codeium Toggle<cr>", { desc = "Toggle Windsurf" })
