b2563aece3090e8fca349b8cad3f44b4b471d625
Jan Van Look <jan.derpingguys@hotmail.com>
2026-05-08 00:25:45 +0200

    add claudecode and change theme

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,31 +1,30 @@
 return {
-  "vyfor/cord.nvim",
-  lazy = false,
-  cmd = {
-    "Cord",
-  },
-  ---@type CordConfig
-  opts = {
-    editor = {
-      client = "nvchad",
-      tooltip = "codemaxxing",
-    },
-    display = {
-      theme = "minecraft",
-      flavor = "accent",
-      view = "full",
-    },
-    text = {
-      workspace = function(opts)
-        local hour = tonumber(os.date "%H")
-        local status = hour >= 22 and "🌙 Late night codemaxxing"
-          or hour >= 18 and "🌆 Evening session"
-          or hour >= 12 and "☀️ Afternoon coding"
-          or hour >= 5 and "🌅 Grinding"
-          or "🌙 Midnight hacking"
+	"vyfor/cord.nvim",
+	lazy = false,
+	cmd = {
+		"Cord",
+	},
+	---@type CordConfig
+	opts = {
+		editor = {
+			tooltip = "codemaxxing",
+		},
+		display = {
+			theme = "minecraft",
+			flavor = "accent",
+			view = "full",
+		},
+		text = {
+			workspace = function(opts)
+				local hour = tonumber(os.date("%H"))
+				local status = hour >= 22 and "🌙 Late night codemaxxing"
+					or hour >= 18 and "🌆 Evening session"
+					or hour >= 12 and "☀️ Afternoon coding"
+					or hour >= 5 and "🌅 Grinding"
+					or "🌙 Midnight hacking"
 
-        return string.format("%s: %s", status, opts.filename)
-      end,
-    },
-  },
+				return string.format("%s: %s", status, opts.filename)
+			end,
+		},
+	},
 }
